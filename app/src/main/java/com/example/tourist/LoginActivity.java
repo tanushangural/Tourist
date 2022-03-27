a34145ccf057822d981da4fbf215a63c68a04f36
Tanush Angural <tanusharya7@gmail.com>
2022-03-28 03:43:15 +0530

    Started UI Design

---
+++ app/src/main/java/com/example/tourist/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.tourist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/tourist/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tourist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
