d21ba91a784e142096b1b67937c46e2cbdcee753
Tanush Angural <tanusharya7@gmail.com>
2022-03-28 09:38:21 +0530

    Started UI Design

--- app/src/main/java/com/example/tourist/InsideGroup.java
+++ app/src/main/java/com/example/tourist/InsideGroup.java
@@ -10,5 +10,6 @@ public class InsideGroup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inside_group);
+
     }
 }
